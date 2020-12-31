25a4831b29b73df2113becaaaaac546d095e74fd
frankmaruf <frankmaruf@hotmail.com>
2020-12-31 18:54:47 +0600

    Cookies in Django

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    path('track_user/',views.track_user,name = 'track_user'),
+    path('stop-tracking/',views.stop_tracking,name = 'stop-tracking'),
+    path('cookie/', views.test_cookie,name='cookie'),
     path('',views.post_list, name='post_list'),
-     path('<pk>/', views.post_detail, name='post_detail'),
+    path('<pk>/', views.post_detail, name='post_detail'),
  #   path('<pk>/', views.post_detail, name='post_detail'),
     path('category/<category_slug>/', views.post_by_category, name='post_by_category'),
     path('tag/<tag_slug>/', views.post_by_tag, name='post_by_tag'),
