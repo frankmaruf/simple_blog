39b85efbf69ca3dbeb3060e51d1a323f75f22b42
frankmaruf <frankmaruf@hotmail.com>
2021-01-05 15:25:09 +0600

    Django password_change() View

--- cadmin/urls.py
+++ cadmin/urls.py
@@ -4,6 +4,8 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.home,name = 'home'),
+    path('password-change-done/',auth_views.PasswordChangeDoneView.as_view(template_name = 'cadmin/password_change_done.html'),name='password_change_done'),
+    path('password-change/',auth_views.PasswordChangeView.as_view(template_name = 'cadmin/password_change.html' ,),name = 'password_change'),
     path('login/', auth_views.LoginView.as_view(template_name='cadmin/login.html'), name = 'login'),
     path('logout/',auth_views.LogoutView.as_view(template_name = 'cadmin/logout.html'),name = 'logout'),
     path('sign_up/', views.sign_up, name="sign-up"),
